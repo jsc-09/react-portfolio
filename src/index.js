59cbd7ffbb2414f14d3a61829f946bc2addb3c45
Joanna Christian <joanna.s.christian@gmail.com>
2022-05-04 13:47:32 -0700

    fixing foundation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/foundation.min.css';
-import 'foundation-sites/dist/css/foundation.min.css';
 
 
 
